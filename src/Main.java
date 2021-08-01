36710dfb3780babeca273695095af2b925d4f04f
Evgeniya Zavyalova <evgeniyazavyalova95@gmail.com>
2021-08-01 18:57:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+      int balance = 78;
+      int replenishment = 1500;
+      int bonus= 0;
+      if (replenishment>1000){
+          bonus = replenishment / 100;
+      }
+      int total = balance+replenishment+bonus;
+      System.out.println(total);
+      System.out.println(bonus);
+    }
+}
